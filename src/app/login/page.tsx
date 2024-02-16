752f73f4ee6faded1b7b82adca5888e83415e2af
akshay.singhal <akshay.singhal@neosoftmail.com>
2024-02-16 13:34:33 +0530

    changes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -39,7 +39,7 @@ const Login = () => {
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
 
                 <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                    Sign in to your account
+                    Sign in to your account New
                 </h2>
             </div>
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
